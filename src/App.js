edd667d2d5362006022af589a32caf9b60a2f0e0
Bikash Dhakal <bikash.dhakal@prabhubank.com>
2022-03-10 11:56:35 +0545

    project architectur setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import "./App.css";
+import Wrapper from "./components/Calculator/Wrapper/Wrapper";
+import Screen from "./components/Calculator/Screen/Screen";
+import ButtonBox from "./components/Calculator/ButtonBox/ButtonBox";
 import Button from "./components/Calculator/Button/Button";
 
 const App = () => {
   return (
-    <Button
-      className=""
-      value="0"
-      onClick={() => {
-        console.log("Button clicked!");
-      }}
-    />
+    <Wrapper>
+      <Screen value="0" />
+      <ButtonBox>
+        <Button
+          className=""
+          value="0"
+          onClick={() => {
+            console.log("Button clicked!");
+          }}
+        />
+      </ButtonBox>
+    </Wrapper>
   );
 };
 
